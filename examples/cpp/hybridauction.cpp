a35f049d4980a3ae444f30a9260ce45ac68d6165
rminton <rminton@uchicago.edu>
2015-09-09 10:45:46 -0500

    Adding hybrid auction example

---
+++ examples/cpp/hybridauction.cpp
@@ -0,0 +1,32 @@
+#include "hybridauction.hpp"
+
+int main()
+{
+  int numBids = 10;
+  int numVals = 10;
+  double weightOnOwnBid = 0.1;
+  
+  HybridAuction ha(numVals,numBids,weightOnOwnBid);
+
+  BCESolver solver(ha);
+  
+  solver.populate();
+  
+  IloCplex cplex = solver.getCplex();
+  cplex.getObjective().setSense(IloObjective::Minimize);
+  cplex.getObjective().setExpr(solver.getObjectiveFunction(2));
+  
+  solver.solve();
+
+  BCEData data;
+  solver.getData(data);
+  
+  stringstream fName;
+  fName << "hybridauction_nv=" << numVals
+	<< "_nb=" << numBids
+	<< "_w=" << weightOnOwnBid << ".bce";
+  string fNameStr = fName.str();
+  const char * fNameC = fNameStr.c_str();
+  BCEData::save(data,fNameC);
+  
+} // main
