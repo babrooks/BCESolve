ff6042ab8bf5a0a19b5921d24ff589682b4d54b9
rminton <rminton@uchicago.edu>
2016-06-28 19:15:00 -0500

    Checked that the license was present at the top of every page.

--- examples/hpp/hybridentryreserve.hpp
+++ examples/hpp/hybridentryreserve.hpp
@@ -1,6 +1,6 @@
 // This file is part of the BCESolve library for games of incomplete
 // information
-// Copyright (C) 2016 Benjamin A. Brooks, Robert J. Minton
+// Copyright (C) 2016 Benjamin A. Brooks and Robert J. Minton
 // 
 // BCESolve free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
