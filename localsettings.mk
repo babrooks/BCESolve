ff4b49dabf02602363023f56133c186464f4a455
Benjamin Brooks <benjaminbrooks@wireless-s1-no-150-19-30.uchicago.edu>
2016-07-22 15:19:52 -0500

    Minor edits to makefiles for compiling on Windows.

--- localsettings.mk
+++ localsettings.mk
@@ -20,9 +20,11 @@
 # ben@benjaminbrooks.net
 # Chicago, IL
 
+BOOSTINCLDIR=/usr/include/boost
+BOOSTDIR=/usr/include/boost
+
 ifneq "$(wildcard /Users/benjaminbrooks/Applications/IBM/ILOG/CPLEX_Studio1262/cplex/bin/x86-64_osx/cplex )" "" 
  $(info Compiling on Bens macbook pro)
- SYSTEM=x86-64_osx
 
  GRBDIR=/Library/gurobi650/mac64
  GRBINCLDIR=$(GRBDIR)/include
@@ -34,10 +36,10 @@ ifneq "$(wildcard /Users/benjaminbrooks/Applications/IBM/ILOG/CPLEX_Studio1262/c
 
  CXX=clang++ -O0
 
+ RMCMD=rm -rf 
 endif
 ifneq "$(wildcard /opt/ibm/ILOG/CPLEX_Studio1251/cplex/bin/x86-64_sles10_4.1/cplex )" "" 
  $(info Compiling on Bens desktop)
- SYSTEM = x86-64_sles10_4.1
 
  GRBDIR=/opt/gurobi563/linux64
  GRBINCLDIR=$(GRBDIR)/include
@@ -48,10 +50,28 @@ ifneq "$(wildcard /opt/ibm/ILOG/CPLEX_Studio1251/cplex/bin/x86-64_sles10_4.1/cpl
  DYNAMIC=-Wl,-Bdynamic
 
  CXX=g++
+ RMCMD=rm -rf 
+endif
+ifneq "$(wildcard C:\Users\babrooks\ )" "" 
+ $(info Compiling on Bens windows laptop)
+
+ GRBDIR=C:\gurobi652\win64
+ GRBINCLDIR=$(GRBDIR)\include
+ GRBLIBDIR=$(GRBDIR)\lib
+ GRBNAME=gurobi65
+
+ BOOSTINCLDIR="C:/Program Files/boost/boost_1_60_0/boost/serialization/"
+ BOOSTDIR="C:/Program Files/boost/boost_1_60_0/"
+
+ STATIC=-Wl,-Bstatic
+ DYNAMIC=-Wl,-Bdynamic
+
+ CXX=g++
+ RMCMD=del 
+
 endif
 ifneq "$(wildcard /opt/ibm/ILOG/CPLEX_Enterprise_Server1262/CPLEX_Studio/cplex/bin/x86-64_linux/cplex )" ""
  $(info Compiling on Robbies laptop)
- SYSTEM = x86-64_linux
 
  GRBDIR=/opt/gurobi650/linux64
  GRBNAME=gurobi65
@@ -60,6 +80,7 @@ ifneq "$(wildcard /opt/ibm/ILOG/CPLEX_Enterprise_Server1262/CPLEX_Studio/cplex/b
  DYNAMIC=-Wl,-Bdynamic
 
  CXX=g++
+ RMCMD=rm -rf 
 endif
 
 # General settings
